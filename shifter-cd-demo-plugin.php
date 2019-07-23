c33c1a71d321a63b45666c1fbc36e3e6ed2500a1
Daniel Olson <emaildano@gmail.com>
2019-07-23 16:35:34 -0400

    First commit

---
+++ shifter-cd-demo-plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://www.getshifter.io
+ * @since             1.0.0
+ * @package           Shifter_CD_Demo_Plugin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Example Continuous Deliver Plugin for Shifter
+ * Plugin URI:        https://github.com/getshifter/shifter-cd-demo-plugin
+ * Description:       An example plugin to demonstrate the Shifter CD git deployment pipeline.
+ * Version:           1.0.0
+ * Author:            DigitalCube
+ * Author URI:        https://www.getshifter.io
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       shifter-cd-demo-plugin
+ * Domain Path:       /languages
+ */
